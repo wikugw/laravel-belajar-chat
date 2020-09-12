79a36408b833bcbf6724a0b1345ae89e2b393c18
wikugw <wikugw@gmail.com>
2020-09-12 10:52:33 +0700

    first commit

---
+++ resources/js/components/MainApp.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="main-app">
+        <chatApp />
+    </div>
+</template>
+<script>
+import chatApp from "./ChatApp";
+export default {
+    components: {
+        chatApp
+    }
+};
+</script>
+<style></style>
